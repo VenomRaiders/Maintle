d991955da4f1c2976e5bc8b47123e4a7524f05d4
LL-Etiane <lukongleinyuyetiane@gmail.com>
2023-03-20 05:32:05 +0100

    feat: investor dashboard layout and struture setup

---
+++ resources/js/Pages/Investor/Index.vue
@@ -0,0 +1,12 @@
+<script setup>
+import InvestorDashboardLayout from '@/Layouts/InvestorDashboardLayout.vue';
+
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Investor Dashboard" />
+    <InvestorDashboardLayout>
+        <h1>Main content here</h1>
+    </InvestorDashboardLayout>
+</template>
