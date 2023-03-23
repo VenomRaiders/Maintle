3e043054153f0fd00c0c5bd1af5b7ce8adc7d228
LL-Etiane <lukongleinyuyetiane@gmail.com>
2023-03-23 16:29:52 +0100

    homepage made responsive

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -8,20 +8,20 @@ import NavBar from "@/Components/NavBar.vue"
 
     <div id="homepage">
         <NavBar />
-        <div class="herro bg-black bg-opacity-70 py-8 flex flex-col items-center">
-            <h1 class="text-white font-bold text-6xl uppercase">Welcome to the maintle</h1>
+        <div class="herro bg-black bg-opacity-70 py-8 px-2 flex flex-col items-center">
+            <h1 class="text-white font-bold text-4xl md:text-6xl uppercase">Welcome to the maintle</h1>
             <div class="">
-                <h1 class="text-yellow-500 font-bold text-2xl my-4 uppercase">Professional Script writers and scripts available</h1>
+                <h1 class="text-yellow-500 font-bold text-xl md:text-2xl my-4 uppercase">Professional Script writers and scripts available</h1>
             <p class="text-white text-justify">A marketing platform for scriptwriters where potential film-makers purchase <br />meaningful scripts for their movies</p>
             </div>
         </div>
 
-        <section class="container mx-auto py-8 flex flex-col items-end space-y-4">
+        <section class="container mx-auto py-8 pr-2 md:pr-0 flex flex-col items-end space-y-4">
             <Link href="#" class="bg-green-300 text-white font-bold rounded-md py-2 text-center w-60">become an investor</Link>
             <Link href="#" class="bg-green-300 text-white font-bold rounded-md py-2 text-center w-60">become a scriptwriter</Link>
         </section>
 
-        <section class="w-2/5 py-16 bg-black bg-opacity-70 flex flex-col items-center justify-center">
+        <section class="w-3/5 md:w-2/5 py-10 md:py-16 bg-black bg-opacity-70 flex flex-col items-center justify-center">
             <Link href="#" class="bg-green-300 text-white font-bold rounded-md py-2 px-4 text-center">browse scripts & projects</Link>
         </section>
     </div>
