7e33f3a5d79f1ade835ee606c2bd3bc83f1988f4
LL-Etiane <lukongleinyuyetiane@gmail.com>
2023-03-23 15:36:37 +0100

    navbar drop down added

--- resources/js/Components/NavBar.vue
+++ resources/js/Components/NavBar.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { Link } from "@inertiajs/vue3"
+import { ref } from "vue"
 import MaintleLogo from "@/Components/MaintleLogo.vue"
+
+const showDropDown = ref(false)
 </script>
 
 <template>
@@ -12,13 +15,18 @@ import MaintleLogo from "@/Components/MaintleLogo.vue"
             <Link href="" class="text-white uppercase">Projects</Link>
             <Link href="" class="text-white uppercase">About us</Link>
             <Link href="" class="text-white uppercase">Blog</Link>
-            <button class="bg-purple-400 text-white uppercase px-2 flex space-x-1">
-                Login
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                </svg>
-            </button>
+            <div class="">
+                <button class="bg-purple-400 text-white uppercase px-2 flex space-x-1">
+                    Login
+                    <svg @click="showDropDown = !showDropDown" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                    </svg>
+                </button>
+
+                <div v-if="showDropDown" id="dropdownitems" class="absolute bg-white mt-0 py-2 px-2">
+                    <Link href="" class="bg-purple-400 text-white px-2">Register</Link>
+                </div>
+            </div>
         </div>
     </nav>
-    <div id="dropdownitems"></div>
 </template>
