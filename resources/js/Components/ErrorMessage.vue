c0631fd33c58c157555c249e304377e50e001ce3
LL-Etiane <lukongleinyuyetiane@gmail.com>
2023-04-02 02:57:30 +0100

    feat: helper components added and form submiting validation error displaying

---
+++ resources/js/Components/ErrorMessage.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800">
+        <slot />
+    </div>
+</template>
--- resources/js/Pages/Auth/RegisterScriptWritter.vue
+++ resources/js/Pages/Auth/RegisterScriptWritter.vue
@@ -1,18 +1,24 @@
 <script setup>
 import { useForm } from '@inertiajs/vue3';
 import AuthProcessLayout from '@/Layouts/AuthProcessLayout.vue';
+import ErrorMessage from "@/Components/ErrorMessage.vue"
+import LoadingComponent from "@/Components/LoadingComponent.vue"
 
 const form = useForm({
-    profilePicture: null,
+    profile_picture: null,
     username: "",
     age: "",
     email: "",
     password: "",
-    passwordConfirmation: "",
+    password_confirmation: "",
     city: "",
     bio: "",
     gender: ""
 })
+
+function createAccount(){
+    form.post("/register/scriptwritter")
+}
 
 </script>
 
@@ -21,21 +27,24 @@ const form = useForm({
         <div class="">
             <h1 class="text-4xl font-bold uppercase my-6">Create Account</h1>
 
-            <form action="" class="border-t-2 border-t-slate-500 py-4">
+            <form @submit.prevent="createAccount" class="border-t-2 border-t-slate-500 py-4">
                 <div class="flex flex-col space-y-2 mb-2">
                     <label for="profilePicture" class="font-bold text-black text-2xl">Upload picture</label>
-                    <input type="file" name="profilePicture" id="profilePicture" @input="$event.target.files[0]">
+                    <input type="file" name="profilePicture" id="profilePicture" @input="form.profile_picture = $event.target.files[0]">
+                    <ErrorMessage v-if="form.errors.profile_picture">{{ form.errors.profile_picture }}</ErrorMessage>
                 </div>
 
                 <div class="flex flex-col md:flex-row md:justify-between space-y-2 md:space-y-0 md:space-x-2 mb-4">
                     <div class="flex flex-col space-y-2 md:w-1/2">
                         <label for="username" class="font-bold text-black text-2xl">Username</label>
                         <input type="text" name="username" id="username" v-model="form.username" class="p-1 outline-none border-2 border-black focus:border-black focus:ring-0">
+                        <ErrorMessage v-if="form.errors.username">{{ form.errors.username }}</ErrorMessage>
                     </div>
                     
                     <div class="flex flex-col space-y-2 md:w-1/2">
                         <label for="age" class="font-bold text-black text-2xl">Age</label>
                         <input type="number" name="age" id="age" v-model="form.age" class="p-1 outline-none border-2 border-black focus:border-black focus:ring-0">
+                        <ErrorMessage v-if="form.errors.age">{{ form.errors.age }}</ErrorMessage>
                     </div>
                 </div>
 
@@ -44,21 +53,24 @@ const form = useForm({
                         <div class="flex flex-col space-y-2 mb-2">
                             <label for="email" class="font-bold text-black text-2xl">Email</label>
                             <input type="email" name="email" id="email" v-model="form.email" class="p-1 outline-none border-2 border-black focus:border-black focus:ring-0">
+                            <ErrorMessage v-if="form.errors.email">{{ form.errors.email }}</ErrorMessage>
                         </div>
 
                         <div class="flex flex-col space-y-2 mb-2">
                             <label for="password" class="font-bold text-black text-2xl">Password</label>
                             <input type="password" name="password" id="password" v-model="form.password" class="p-1 outline-none border-2 border-black focus:border-black focus:ring-0">
+                            <ErrorMessage v-if="form.errors.password">{{ form.errors.password }}</ErrorMessage>
                         </div>
 
                         <div class="flex flex-col space-y-2 mb-2">
                             <label for="passwordConfirmation" class="font-bold text-black text-2xl">Confirm password</label>
-                            <input type="password" name="passwordConfirmation" id="passwordConfirmation" v-model="form.passwordConfirmation" class="p-1 outline-none border-2 border-black focus:border-black focus:ring-0">
+                            <input type="password" name="passwordConfirmation" id="passwordConfirmation" v-model="form.password_confirmation" class="p-1 outline-none border-2 border-black focus:border-black focus:ring-0">
                         </div>
 
                         <div class="flex flex-col space-y-2 mb-2">
                             <label for="city" class="font-bold text-black text-2xl">City</label>
                             <input type="Text" name="city" id="city" v-model="form.city" class="p-1 outline-none border-2 border-black focus:border-black focus:ring-0">
+                            <ErrorMessage v-if="form.errors.city">{{ form.errors.city }}</ErrorMessage>
                         </div>
 
                         <div class="flex flex-col space-y-2 my-4">
@@ -80,10 +92,12 @@ const form = useForm({
                         <div class="flex flex-col space-y-2">
                             <label for="bio" class="font-bold text-black text-2xl">Bio</label>
                             <textarea name="bio" id="bio" v-model="form.bio" rows="10"></textarea>
+                            <ErrorMessage v-if="form.errors.bio">{{ form.errors.bio }}</ErrorMessage>
                         </div>
 
                         <div class="flex justify-center">
-                            <input type="submit" value="Confirm" class="rounded-md bg-primary text-white font-bold py-2 px-4 cursor-pointer">
+                            <LoadingComponent v-if="form.processing" class="border-2 border-primary"/>
+                            <input type="submit" value="Confirm" v-else class="rounded-md bg-primary text-white font-bold py-2 px-4 cursor-pointer">
                         </div>
                     </div>
                 </div>
