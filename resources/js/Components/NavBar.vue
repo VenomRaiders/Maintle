cf5ae2e038b1790c119c7c80c2fe01ed448cb7e2
LL-Etiane <lukongleinyuyetiane@gmail.com>
2023-04-02 04:11:50 +0100

    navlinks updated to display appropriately depending on login state

--- resources/js/Components/NavBar.vue
+++ resources/js/Components/NavBar.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { Link } from "@inertiajs/vue3"
-import { ref } from "vue"
+import { Link,usePage } from "@inertiajs/vue3"
+import { ref, computed } from "vue"
 import MaintleLogo from "@/Components/MaintleLogo.vue"
 
 const showMobileNav = ref(false)
 const showDropDown = ref(false)
+const user = computed(() => usePage().props.auth.user)
 </script>
 
 <template>
@@ -16,23 +17,28 @@ const showDropDown = ref(false)
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
                 </svg>
             </button>
-            <div id="links" :class="{'hidden md:flex': !showMobileNav}" class="flex flex-col md:flex-row md:space-x-8 py-2 text-md">
+            <div id="links" :class="{'hidden md:flex': !showMobileNav}" class="flex flex-col md:flex-row md:space-x-8 items-center py-2 text-md">
                 <Link href="" class="text-white uppercase">Home</Link>
                 <Link href="" class="text-white uppercase">Scripts</Link>
                 <Link href="" class="text-white uppercase">Projects</Link>
                 <Link href="" class="text-white uppercase">About us</Link>
                 <Link href="" class="text-white uppercase">Blog</Link>
-                <div class="">
+                <div v-if="!user" class="">
                     <button class="bg-purple-400 text-white uppercase px-2 flex space-x-1">
-                        Login
+                        <Link :href="route('login')">Login</Link>
                         <svg @click="showDropDown = !showDropDown" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                         </svg>
                     </button>
 
                     <div v-if="showDropDown" id="dropdownitems" class="absolute bg-white mt-0 py-2 px-2">
-                        <Link href="" class="bg-purple-400 text-white px-2">Register</Link>
+                        <Link :href="route('register.scriptwritter')" class="bg-purple-400 text-white px-2">Register</Link>
                     </div>
+                </div>
+
+                <div v-if="user" class="flex flex-col md:flex-row md:space-x-8 items-center">
+                    <Link :href="route('dashboard')" class="text-white uppercase">Dashboard</Link>
+                    <Link :href="route('logout')" as="button" method="POST" class="text-white uppercase bg-primary p-2 rounded-md">Logout</Link>
                 </div>
             </div>
         </div>
