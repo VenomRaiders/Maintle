31c2d93fb71a167873df932fca7587d2d14beed7
LL-Etiane <lukongleinyuyetiane@gmail.com>
2023-04-14 05:00:50 +0100

    feat: creates scriptwriter and catalog page

---
+++ resources/js/Pages/Catalog.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Head, Link } from "@inertiajs/vue3"
+import NavBar from "@/Components/NavBar.vue"
+</script>
+
+<template>
+    <Head title="Catalog"/>
+
+    <div id="catalog">
+        <NavBar />
+        
+    </div>
+</template>
---
+++ resources/js/Pages/ScriptWriters.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { Head, Link } from "@inertiajs/vue3"
+import NavBar from "@/Components/NavBar.vue"
+
+const props = defineProps(['scriptWriters','okay'])
+console.log(props.scriptWriters)
+</script>
+
+<template>
+    <Head title="Script Writters"/>
+
+    <div id="catalog">
+        <NavBar class="bg-primary"/>
+        
+        <div class="">
+            {{ scriptWriters }}
+            <h1 class="text-center text-4xl font-bold my-4">Our Verified Script Writers</h1>
+            <div class="">
+                <div v-for="scriptWriter in scriptWriters" :key="scriptWriter.id" class="">
+                    {{ scriptWriter.username }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
