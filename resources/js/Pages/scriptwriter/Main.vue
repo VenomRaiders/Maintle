c7a05a6182364892954272d1e9f8732fffcc935d
HanslettTheDev <HanslettTheDev@gmail.com>
2023-05-03 09:36:21 +0100

    ref(ui): applied the stashed changes from the conflicted branches. This commit updates the scriptwritter dashboard. changes not completed yet

--- resources/js/Pages/scriptwriter/Main.vue
+++ resources/js/Pages/scriptwriter/Main.vue
@@ -1,6 +1,6 @@
 <script setup>
 import ScriptNavBar from '@/Components/ScriptNavBar.vue';
-import ScriptsCard from '@/Components/ScriptsCard.vue';
+import ScriptWriterDashboardLayout from '@/Layouts/ScriptWriterDashboardLayout.vue';
 import StandardButton from '@/Components/StandardButton.vue';
 
 const props = defineProps(['scripts'])
@@ -9,21 +9,17 @@ const props = defineProps(['scripts'])
 
 <template>
   <ScriptNavBar>
-    <div>
-      <StandardButton text="New Script" :isLink=true :href="route('scriptwriter.add_script')"></StandardButton>
-    </div>
-    <div class="all-scripts">
-      <div class="flex flex-wrap">
-        <ScriptsCard v-for="script in scripts" :key="script.id" 
+    <StandardButton text="New Script" :isLink=true :href="route('scriptwriter.add_script')"></StandardButton>
+    <div class="content">
+      <ScriptWriterDashboardLayout v-for="script in scripts" :key="script.id" 
         :stitle= "script.script_title"
         :body="script.script_synopsis"
         :genres="script.genres"
         >
         <img :src="'/storage/'+script.poster_image" alt="Script offical blopper image" style="width:100%">
-      </ScriptsCard>
-      </div>
+      </ScriptWriterDashboardLayout>
     
-    <p v-if="!scripts">No Scripts Have been Added</p>
+      <p v-if="scripts.length === 0">No Scripts Have been Added</p>
     
     </div>
   </ScriptNavBar>
@@ -32,52 +28,35 @@ const props = defineProps(['scripts'])
 
 <style scoped>
 
+
+.content {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 30px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+
 .btn {
-  width: fit-content;
-}
-.all-scripts {
-  margin-top: 30px;
-  width: 100%;
-  overflow-y: auto;
-  padding: 10px;
-  max-height: 790px;
-  display: flex;
-  flex-wrap: wrap;
-  gap: 50px;
+    width: fit-content;
+    margin-top: 15px;
 }
 
-/* Media queries for the responsive layout */
+/* Scaling for desktop devices */
 
-@media only screen and (min-width: 1050px) {
-  .all-scripts .card {
-    flex-grow: 0.4;
-  }
+@media only screen and (min-width: 690px) {
+    .content {
+        justify-content: center;
+    }
+    .card {
+        width: 100%;
+    }
 }
 
-/* 1200px -> 1700px */
-@media only screen and (min-width: 1200px) { 
-
-  .all-scripts {
-    max-height: 590px;
-    overflow-y: auto;
-  }
-  .all-scripts .card {
-    margin-right: 5px;
-    margin-bottom: 5px;
-  }
-
-  .card img {
-    height: 300px;
-  }
-  
-  .all-scripts .card {
-    flex-grow: 1;
-  }
-}
-
-@media only screen and (min-width: 1307px) {
-  .all-scripts .card {
-    flex-grow: 0.4;
-  }
+@media only screen and (min-width: 1200px) {
+    .content {
+        padding-top: 30px;
+        gap: 50px;
+    }
 }
 </style>
