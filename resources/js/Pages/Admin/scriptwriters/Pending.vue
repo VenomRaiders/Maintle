17d29ad3aa78a4e440188cfa9e800b3f407d8da3
HanslettTheDev <HanslettTheDev@gmail.com>
2023-05-03 09:03:29 +0100

    ref: fixing scriptwriter dashboard

---
+++ resources/js/Pages/Admin/scriptwriters/Pending.vue
@@ -0,0 +1,10 @@
+<script setup>
+import ScriptwritersAdmin from "@/Components/ScriptwritersAdmin.vue";
+import StandardButton from "@/Components/StandardButton.vue";
+</script>
+
+<template>
+<ScriptwritersAdmin>
+   
+</ScriptwritersAdmin>
+</template>
