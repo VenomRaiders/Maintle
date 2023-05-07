e32dc168b7407d02add2ebbcfa6d690e8daa3e9b
LL-Etiane <lukongleinyuyetiane@gmail.com>
2023-05-07 19:42:05 +0100

    feat: amounts length on projects table updated

--- database/migrations/2023_05_03_153849_add_cost_and_contribution_to_projects_table.php
+++ database/migrations/2023_05_03_153849_add_cost_and_contribution_to_projects_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->decimal('amount', 6, 2)->nullable()->default(0.00);
-            $table->decimal('contribution', 6, 2)->nullable()->default(0.00);
+            $table->decimal('amount', 15, 2)->nullable()->default(0.00);
+            $table->decimal('contribution', 15, 2)->nullable()->default(0.00);
             $table->boolean('is_funded')->default(false);
         });
     }
